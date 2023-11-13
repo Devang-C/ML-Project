f709bff701e0279303a3d4955c6df3488e6fcf95
Devang-C <devangchavan0204@gmail.com>
2023-11-13 23:10:58 +0530

    added hyperparameter tuining

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -54,7 +54,49 @@ class Model_Trainer():
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            model_report = evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+            # Adding hyperparameter tuining for each ml model
+
+            # we can basically add this hyperparameters to a config file such as yaml file
+            # so we dont need to write it here we can access it from the file itself
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            model_report = evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,param=params)
 
             best_model_score = max(sorted(model_report.values()))
 
@@ -66,7 +108,7 @@ class Model_Trainer():
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
+            logging.info(f"Best found model on both training and testing dataset which is {best_model}")
 
             save_obj(
                 filepath=self.model_trainer_config.trained_model_file_path,
--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,8 @@ import dill
 
 from sklearn.metrics import r2_score
 
+from sklearn.model_selection import GridSearchCV
+
 def save_obj(filepath,obj):
     try:
         dir_path = os.path.dirname(filepath)
@@ -23,13 +25,20 @@ def save_obj(filepath,obj):
 
 
 
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
 
+            # adding the hyperparameter tuining
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
 
             y_predicted = model.predict(x_test)
